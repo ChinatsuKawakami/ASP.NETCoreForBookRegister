6e8a451c5c4cb073af7f36647ef6f350fde1f12e
ChinatsuKawakami <ckawakam@my.centennialcollege.ca>
2017-08-03 12:40:38 -0400

    0.0.4 add loginForm.html in Project

--- BookReader/Program.cs
+++ BookReader/Program.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Hosting;
  Description : this is the web applilcation to register books users like
  Author: Chinatsu Kawakami
  Created Date: 2nd August 2017
- Versiton : 0.0.3 add navigation bar in index.html  
+ Versiton : 0.0.4 add loginForm.html in Project  
  */
 namespace BookReader
 {
